da97b2eae03957849b928b8a83de3459f997c251
margonztrejo <mar.gonz.trejo@gmail.com>
2021-03-24 21:34:24 -0600

    version 1.0.1

--- app/src/main/java/com/example/mynewlibrary/MessageMaker.kt
+++ app/src/main/java/com/example/mynewlibrary/MessageMaker.kt
@@ -2,6 +2,6 @@ package com.example.mynewlibrary
 
 class MessageMaker(private val name: String) {
     fun getMessage(): String{
-        return "Hola $name, te escribo desde mi librería Android"
+        return "Hola $name, te escribo desde mi librería Android de versión 1.0.1"
     }
 }
